64c0b72870b37bbd22ddcf8fe3b3ca1666c74329
nila_wilda <nilawildaalaluf@gmail.com>
2022-06-17 14:32:57 +0700

    testing

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var express = require("express");
 var app = express();
-const router = express.Router();
 const PORT = process.env.PORT || 5050;
 const { books } = require("./handlers/books");
 
-router.get("/books", books);
-
-router.get("/", (req, res) => {
+app.get("/books", books);
+app.get("/", (req, res) => {
   res.send("This is my demo project");
 });
+
+app.listen(PORT, function () {
+  console.log(`Demo project at: ${PORT}!`);
+});
+
+// app.get("/", (req, res) => {
+// res.send("This is my demo project");
+// });
 
 // app.use("/books", books);
 
 // module.exports.handler = serverless(app);
 
-app.listen(PORT, function () {
-  console.log(`Demo project at: ${PORT}!`);
-});
+// app.listen(PORT, function () {
+//   console.log(`Demo project at: ${PORT}!`);
+// });
